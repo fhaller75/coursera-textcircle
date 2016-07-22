c429e0ab98310a4b1d9e4109c67be09c08c96bb1
Frédéric Haller <fhaller75@gmail.com>
2016-07-22 19:08:50 +0200

    After lesson 3

--- textcircle.js
+++ textcircle.js
@@ -1,9 +1,26 @@
-if (Meteor.isClient){
+this.Documents = new Mongo.Collection("documents");
 
+if (Meteor.isClient){
+    Template.editor.helpers({
+        docid:function(){
+            console.log("In docid editor helper")
+            console.log(Documents.findOne());
+            var doc = Documents.findOne();
+            if (doc){
+                return doc._id;
+            }
+            else {
+                return undefined;
+            }
+        }
+    });
 }
 
 if (Meteor.isServer){
 	Meteor.startup(function(){
 		// code to run on server at startup
+    if (!Documents.findOne()){
+        Documents.insert({title:"my new document"})
+    }
 	})
 }
