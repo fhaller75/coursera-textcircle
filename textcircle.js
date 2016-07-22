f97e4c878572ee32c5d6965bacd2855b2697b761
Frédéric Haller <fhaller75@gmail.com>
2016-07-22 22:21:49 +0200

    After lesson 5: Session

--- textcircle.js
+++ textcircle.js
@@ -1,10 +1,20 @@
 this.Documents = new Mongo.Collection("documents");
 
 if (Meteor.isClient){
+
+    Meteor.setInterval(function(){
+        Session.set("current_date", new Date());
+    }, 1000);
+
+    Template.date_display.helpers({
+        current_date:function(){
+            return Session.get("current_date");
+        }
+    });
+
     Template.editor.helpers({
         docid:function(){
-            console.log("In docid editor helper")
-            console.log(Documents.findOne());
+            // console.log("In docid editor helper")
             var doc = Documents.findOne();
             if (doc){
                 return doc._id;
@@ -19,8 +29,8 @@ if (Meteor.isClient){
 if (Meteor.isServer){
 	Meteor.startup(function(){
 		// code to run on server at startup
-    if (!Documents.findOne()){
-        Documents.insert({title:"my new document"})
-    }
-	})
+        if (!Documents.findOne()){
+            Documents.insert({title:"my new document"})
+        }
+	});
 }
