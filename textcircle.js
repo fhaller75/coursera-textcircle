b7bb4dde55082ee2ef055a15596e1c5b125c0829
Frédéric Haller <fhaller75@gmail.com>
2016-07-23 00:07:45 +0200

    After lesson 8: HTML preview

--- textcircle.js
+++ textcircle.js
@@ -4,13 +4,22 @@ if (Meteor.isClient){
 
     Template.editor.helpers({
         docid:function(){
-            // console.log("In docid editor helper")
+            // console.log("In docid editor helper");
             var doc = Documents.findOne();
             if (doc){
                 return doc._id;
             }
             else {
                 return undefined;
+            }
+        },
+        config:function(){
+            // console.log("In config editor helper");
+            return function(editor){
+                editor.on("change", function(cm_editor, info){
+                    // console.log(cm_editor.getValue());
+                    $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+                });
             }
         }
     });
